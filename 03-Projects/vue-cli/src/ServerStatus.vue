67d82877712d1858b559e0ad68a5cab832683d3d
David Gutierrez <dcgutierrez93@yahoo.com>
2018-10-25 12:19:35 -0500

    Added Folder structures

---
+++ 03-Projects/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<!-- Create a new component -->
+<template>
+    <div>
+        <p>Server Status: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Normal';
+            }
+        }
+    }
+</script>
--- 03-Projects/vue-cli/src/main.js
+++ 03-Projects/vue-cli/src/main.js
@@ -7,9 +7,23 @@ import Home from './Home.vue'
 // Run Dev
 // $ npm run dev
 
-Vue.component('app-server-status', Home);
+Vue.component('app-servers', Home);
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+// Whilst it also might work fine in bigger projects, there's also an alternative you might want to consider.
+//
+// Instead of having your components in a components/ folder (and storing other shared files in other folders - e.g. shared/), you can also group your files by feature.
+//
+// This could look like this:
+//
+// - main.js
+// - users/
+// - - account/
+// - - analytics/
+// - shop
+// - - main/
+// - - checkout/
