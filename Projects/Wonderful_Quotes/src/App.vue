90460d7ea2a7e9a3a882ee2760f218e39a81c8f5
David Gutierrez <dcgutierrez93@yahoo.com>
2018-10-26 10:17:08 -0500

    Added Components Quote

---
+++ Projects/Wonderful_Quotes/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+          quotes: [
+                'Just a quote to see something'
+          ],
+          maxQuotes: 10
+      }
+    }
+  }
+</script>
