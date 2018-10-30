31b46ac2906dfeb86b521d3b53eecbc582410f18
David Gutierrez <dcgutierrez93@yahoo.com>
2018-10-30 10:15:45 -0500

    Adding Advanced Components

---
+++ 06-components/components_communicating/src/components/UserDetail.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    // To Receive data from parent we use props property
+    props: {
+        name: String
+        // Validating properties
+        // name: [String, Array]
+    },
+    methods: {
+        switchName () {
+            return this.name.split("").reverse().join("");
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
---
+++ Projects/Wonderful_Quotes/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <h3>Quote Added</h3>
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="progress">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
